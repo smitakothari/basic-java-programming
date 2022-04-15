92f20b0f0eec02d11fe411583c173ef8b7f0cf0d
Smita Kothari <smita.s.kothari@gmail.com>
2022-04-15 14:31:19 -0500

    Added Hello program

---
+++ src/CodePractice/Hello.java
@@ -0,0 +1,15 @@
+package src.CodePractice;
+
+/*
+Write a Java program to print 'Hello' on screen and then print your name on a separate line.
+Expected Output :
+Hello
+Alexandra Abramov
+ */
+
+public class Hello {
+    public static void main(String args[]){
+        System.out.println("Hello");
+        System.out.println("Alexandra Abramov");
+    }
+}
